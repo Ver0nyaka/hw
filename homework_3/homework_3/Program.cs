99dc4b70789146fea23cb48f2f01bbd50d3390cc
Veronika <ver0nyaka@mail.ru>
2021-12-28 19:59:14 +0700

    fnished

---
+++ homework_3/homework_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace homework_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matr = new int[2, 2] { { 1, 2 }, { 3, 4 } };
+
+            Console.WriteLine($"{matr[0, 0]}");
+            Console.WriteLine($" {matr[0, 1]}");
+            Console.WriteLine($"  {matr[1, 0]}");
+            Console.WriteLine($"   {matr[1, 1]}");
+
+        }
+    }
+}
---
+++ homework_3/z_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace z_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string hello = Console.ReadLine();
+            char[] olleh = hello.ToCharArray();
+            int last = olleh.Length - 1;
+            for (int i = last; i >= 0; i--)
+            {
+                Console.Write(olleh[i]);
+            }
+
+
+
+        }
+    }
+}
